e1077bd26ef441d7cddb4a59007111c2d0a4f860
VictorMoshkov <grot-mvm@mail.ru>
2021-09-19 22:15:34 +0300

    Initial commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+    public  String name;
+    public  String post;
+    public  String email;
+    public  String telephone;
+    public int salary;
+    public int age;
+    public Employee(String name, String post, String email, String telephone, int salary, int age){
+        this.name = name;
+        this.post = post;
+        this.email = email;
+        this.telephone = telephone;
+        this.age = age;
+        this.salary = salary;
+
+    }
+
+    //public Employee(String name, String post, String email, String telephone, String salary, String age) {
+
+    //}
+    public void showInf(){
+        System.out.println(name + " " + post + " " + email + " " + telephone + " " + salary + " " + age);
+    }
+
+
+    public int getAge(){
+        return age;
+
+    }
+
+}
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        Employee[] person = new Employee[5];
+        person[0] = new Employee("Иванов Иван Иванович","Уборщик", "ivanov@mail.ru", "898711111", 10000, 40);
+        person[1] = new Employee("Сергеев Сергей Сергеевич","Программист", "ser@mail.ru", "89873333", 20000, 34);
+        person[2] = new Employee("Андреев Андрей Анреевич ","Тестировщик", "andr@mail.ru", "898744444", 30000, 56);
+        person[3] = new Employee("Костин Константин Константинович","Дизайнер", "kost@mail.ru", "89875555", 40000, 21);
+        person[4] = new Employee("Алексеев Алексей Алексеевич","Бухгалтер", "aleks@mail.ru", "898766666", 50000, 43);
+        //person[2].showInf();
+
+        for (int i = 0; i < person.length; i++ ){
+            if ((person[i].getAge() > 40)){
+                person[i].showInf();
+
+            }
+
+        }
+    }
+
+}
+
